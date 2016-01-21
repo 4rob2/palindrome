d3381331fd8ef9fe3d089d1ed513b99c04f82219
David Remington and rob reasoner <david.ian.remington@gmail.com>
2016-01-20 16:44:43 -0800

    add html front end

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,54 @@
 
-var isPalindrome = function (word) {
+$(function(){
+  $("form#word-group").submit(function(event) {
 
-  var preWord = []
-  var postWord = []
 
-  var compareLast = function (word) {
-    var wordLength = word.length;
-    var lastChar = word.charAt(wordLength -1);
-    if (lastChar !== word[0]) {
-      return 'not a palindrome';
-    } else if (lastChar === word[0]) {
-        isEven(word)
-      };
-  };
 
-    function isEven(wordLength) {
-      if (wordLength % 2 === 0) {
-        return true;}
-      else {
-        return false;
-        midPoint(word);
-      };
-  };
+  // var isPalindrome = function (word) {
+    var word = $("input#word").val();
+    var preWord = [];
+    var postWord = [];
 
-    function midPoint(word) {
+    alert(word);
+
+    var compareLast = function(word) {
       var wordLength = word.length;
-      var middle = word[((wordLength-1)/2)];
-      var preWord = word.slice(0, (word.indexOf(middle)));
-      var postWord = word.slice((word.indexOf(middle) + 1), wordLength);
-      return postWord;
+      var lastChar = word.charAt(wordLength -1);
+      if (lastChar !== word[0]) {
+        $("#results").empty().append("not a palindrome");
+        $("#nextinput").show();
+      } else if (lastChar === word[0]); {
+          isEven(word);
+        };
     };
 
-    function finalTest(word) {
-      if (preWord === postWorld) {
-      return ("this is a palindrome");
-    } else {
-      return ("this is not a palindrome")
+      function isEven(wordLength) {
+        if (wordLength % 2 === 0) {
+          return true;}
+        else {
+          return false;
+          midPoint(word);
+        };
     };
-  };
 
-};
+      function midPoint(word) {
+        var wordLength = word.length;
+        var middle = word[((wordLength-1)/2)];
+        var preWord = word.slice(0, (word.indexOf(middle)));
+        var postWord = word.slice((word.indexOf(middle) + 1), wordLength);
+        return postWord;
+      };
+
+      function finalTest(word) {
+        if (preWord === postWord) {
+        return ("this is a palindrome");
+      } else {
+        return ("this is not a palindrome")
+      };
+
+    };
+  // };
+
+  event.preventDefault();
+});
+});
